4f93902aa1ed9d344785b2b26c1824ea330d7561
Ogoa <ogopabhs@gmail.com>
2023-08-14 15:44:16 -0700

    Remove unused variables in 102-print_comb5.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,19 +11,14 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (i < j)
-			{
-				putchar(i + 48);
-				putchar(j + 48);
-				if (i == 8)
-					continue;
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(i + 48);
+			putchar(j + 48);
+			putchar(',');
+			putchar(' ');
 		}
+		putchar('\n');
 	}
-	putchar('\n');
 	return (0);
 }
