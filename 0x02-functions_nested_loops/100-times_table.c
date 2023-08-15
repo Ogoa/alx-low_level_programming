7071889b6515b1543727d2bd8e287bc917b19856
Ogoa <ogopabhs@gmail.com>
2023-08-15 13:47:12 -0700

    Attempt to correct task 12 file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,31 +9,36 @@ void print_times_table(int n)
 	int i, j, k;
 
 	if (n > 15 || n < 0)
-		break;
-	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		break;
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if (j == 0)
-				_putchar('0' + k);
-			if (k < 10 && j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + k);
+				k = i * j;
+				if (j == 0)
+					_putchar('0' + k);
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + k);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (k / 10));
+					_putchar('0' + (k % 10));
+				}
 			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (k / 10));
-				_putchar('0' + (k % 10));
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
