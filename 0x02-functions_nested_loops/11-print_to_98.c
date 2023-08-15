f3b3b82644842e6c1c4ca67520fd3760333904ee
Ogoa <ogopabhs@gmail.com>
2023-08-15 12:35:18 -0700

    Attempt to correct task 9 and task 11 files

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,22 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 98; i++)
+	switch (i)
 	{
-		printf("%d, ", i);
+	case i < 98:
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		break;
+	case i > 98:
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		break;
+	default:
+		printf("98\n");
 	}
 	printf("98\n");
 }
