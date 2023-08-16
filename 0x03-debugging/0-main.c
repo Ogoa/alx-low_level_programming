f7a125c76e36920a6adf7e1fc131f198777f4c74
Ogoa <ogopabhs@gmail.com>
2023-08-16 04:07:56 -0700

    Complete task 0 of C Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Test function for its output
+ * Return: Always 0 (Successful exit)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
