50d4e442e3a66c72a986303309426d510401649e
Ogoa <ogopabhs@gmail.com>
2023-08-21 17:05:55 -0700

    Attempt task 5 of C Pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be reversed
+ */
+void rev_string(char *s)
+{
+	char *reverse;
+	int i = 0;
+	int j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		*(reverse + j) = *(s + i);
+		i--;
+		j++;
+	}
+}
