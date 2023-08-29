2c30041c5157698df4171ec978810aa3f0bdd591
Ogoa <ogopabhs@gmail.com>
2023-08-29 01:35:34 +0000

    Correct task 8 to comply to betty documentation standards

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,6 +2,8 @@
 
 /**
  * set_string - Sets the value of a pointer to a char
+ * @s: The pointer to the string to be changed
+ * @to: The pointer to the string being assigned to 's'
  */
 void set_string(char **s, char *to)
 {
