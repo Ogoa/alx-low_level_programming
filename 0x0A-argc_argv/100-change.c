444a2e5b6f4506c049ef70a0981d7cd5737fe34a
Ogoa <ogopabhs@gmail.com>
2023-09-01 18:53:36 +0000

    Correct errors in task 5 of C argc/argv
    Previously, (*(argv + 1) < 0) in the file 100-change.c was causing a
    comparison between a pointer and an integer value; something that is
    logically incorrect.
    This was changed to (**(argv + 1) < 0).

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	int change;
 
 	if (argc != 2)
@@ -20,7 +19,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (*(argv + 1) < 0)
+	if (**(argv + 1) < 0)
 	{
 		printf("0\n");
 		return (0);
