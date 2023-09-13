ec842dffac9eaa844f5f2b122606fa5669283a61
Ogoa <ogopabhs@gmail.com>
2023-09-13 12:57:32 +0000

    Complete task 0 of C Function Pointers
    This directory was created for the "0x0F. C - Function Pointers"
    project.
    A readme was also created for the directory.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: Pointer to the memory location of the string being printed
+ * @f: Pointer to a function that takes a pointer to a character
+ * argument and returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
