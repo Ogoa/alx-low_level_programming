91156c70ba5f5c4836ebb725106c71aa8cf0abbf
Ogoa <ogopabhs@gmail.com>
2023-10-02 20:37:20 +0000

    Complete task 0 of File I/O
    This direcctory was created alongside it's readme file and a main.h
    header file.
    0-read_textfile.c was also created for task 0 of the project.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Pointer to the string containing the file path and name
+ * @letters: The number of letter to be read and printed
+ *
+ * Return: The actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t bytes_read = 0;
+	ssize_t bytes_written = 0;
+	int f_descriptor;
+	char buffer[1024];
+
+	if (filename == NULL)
+		return (0);
+	f_descriptor = open(filename, O_RDONLY);
+	if (f_descriptor == -1)
+		return (0);
+	bytes_read = read(f_descriptor, buffer, letters);
+	if (bytes_read == -1)
+		return (-1);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+		return (0);
+	return (bytes_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,12 @@
+This directory contains tasks under the `0x15. C - File I/O` project.<br>
+- No global variables were used.
+- The only C standard library functions allowed were `malloc`, `free` and `exit`.
+- The only allowed system calls were `read`, `write`, `open` and `close`.
+
+
+`main.h`<br>
+This header file contains all function prototypes of the functions used across all source files in this directory.
+
+`0-read_textfile.c`<br>
+Task 0<br>
+This program contains a function that reads a text file and prints it to the `POSIX` standard output, then returns the actual number of letters it could read and print.
