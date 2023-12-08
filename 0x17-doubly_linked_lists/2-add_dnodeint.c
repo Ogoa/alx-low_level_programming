83b1d37fac625cc9f87731be9dcd6dedda5e534f
Ogoa <ogopabhs@gmail.com>
2023-12-08 22:08:45 +0000

    Update: Make 2-add_dnodeint handle edge cases

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,12 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-	if (!head || !*head)
+	if (head == NULL || *head == NULL)
 	{
 		*head = new_node;
-		return (new_node);
 	}
-	new_node->next = *head;
-	*head = new_node;
+	else
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 	return (new_node);
 }
