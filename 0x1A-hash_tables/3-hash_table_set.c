3268f9c5e464360717b76a67fe28857e37c828e2
Ogoa <ogopabhs@gmail.com>
2023-12-22 13:07:58 +0000

    Update: Duplicate value string in 3-hash_table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
