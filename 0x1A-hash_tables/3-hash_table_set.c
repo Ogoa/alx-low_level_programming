45e01cdc42782faa87266de2328ea29b51bee106
Ogoa <ogopabhs@gmail.com>
2023-12-28 13:45:43 +0000

    Update: Handle updating value of duplicate key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,21 +14,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	unsigned long int index; /* Hash value returned by the hash function */
+	unsigned long int i = 0;
+	char *value_copy;
 
-	if (ht == NULL || key == NULL) /**
+	if (ht == NULL || key == NULL || *key == '\0') /**
 			  * Handle a NULL pointer to the hash table
 			  * and an empty @key string
 			  */
 		return (0);
 	/* Allocate memory for the element being added */
+	value_copy = strdup(value);
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (new_node == NULL || value_copy == NULL)
 		return (0);
-	new_node->key = (char *)key;
+	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+	    if (strcmp(ht->array[i]->key, key) == 0)
+	    {
+	        free(ht->array[i]->value);
+	        ht->array[i]->value = value_copy;
+	        return (1);
+	    }
+	}
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
